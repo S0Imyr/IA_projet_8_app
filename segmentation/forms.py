3d1f45030b88a3ed909a8be47a01b100eeb5a13a
rlossec <raph.lossec@gmail.com>
2023-03-26 20:28:44 +0200

    Version 2

--- segmentation/forms.py
+++ segmentation/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 
+MODEL_CHOICES = (('FPN', 'FPN sans augmentation'),
+                 ('FPN_AUG', 'FPN avec augmentation'),
+                 ('UNET', 'U-Net sans augmentation'),
+                 ('UNET_AUG', 'U-Net avec augmentation'),
+                )
+
 
 class ImageMaskForm(forms.Form):
     image = forms.ImageField()
     mask = forms.ImageField(required=False)
+    model = forms.ChoiceField(choices=MODEL_CHOICES, widget=forms.RadioSelect)
--- segmentation/urls.py
+++ segmentation/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('segmentation/', views.segmentation, name='segmentation'),
+
+    path('', views.home, name='home'),
+    path('models', views.models, name='models'),
+    path('segmentate/', views.segmentation, name='segmentation'),
+    path('about', views.about, name='about'),
 ]
